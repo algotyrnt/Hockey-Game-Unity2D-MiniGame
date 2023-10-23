0f82fa7858e5a415883670658d5d22b6831f3d6c
algotyrnt <punjitha@gmail.com>
2023-10-23 20:17:08 +0530

    First commit, empty project and .gitignore

---
+++ Assets/Scripts/InterstitialAdsScript.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Advertisements;
+using UnityEngine.SceneManagement;
+
+public class InterstitialAdsScript : MonoBehaviour, IUnityAdsListener
+{
+    string gameId = "3864325";
+    bool testMode = true;
+
+    public int scenIndex;
+
+    void Start()
+    {
+        Advertisement.AddListener(this);
+        Advertisement.Initialize(gameId, testMode);
+    }
+    
+    public void ShowInterstitialAd()
+    {
+        if (Advertisement.IsReady())
+        {
+            Advertisement.Show("video");
+        }
+        else
+        {
+            LoadLevel(1);
+        }
+    }
+
+    public void OnUnityAdsDidFinish(string surfacingId, ShowResult showResult)
+    {
+        // Define conditional logic for each ad completion status:
+        if (showResult == ShowResult.Finished)
+        {
+            LoadLevel(1);
+        }
+        else if (showResult == ShowResult.Skipped)
+        {
+            LoadLevel(1);
+        }
+        else if (showResult == ShowResult.Failed)
+        {
+            LoadLevel(1);
+        }
+    }
+
+    public void OnUnityAdsReady(string placementId)
+    {
+        
+    }
+
+    public void OnUnityAdsDidError(string message)
+    {
+        
+    }
+
+    public void OnUnityAdsDidStart(string placementId)
+    {
+        
+    }
+
+    public void OnDestroy()
+    {
+        Advertisement.RemoveListener(this);
+    }
+
+    public void LoadLevel(int sceneIndex)
+    {
+        SceneManager.LoadScene(sceneIndex);
+    }
+
+}
---
+++ Assets/Scripts/MenuScene/MenuManager.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MenuManager : MonoBehaviour
+{
+
+    public Toggle MultiplayerToggle;
+    public GameObject DifficultyToggles;
+    public InterstitialAdsScript adsScript;
+
+    private void Start()
+    {
+        MultiplayerToggle.onValueChanged
+            .AddListener(isMultiplayerOn => DifficultyToggles.SetActive(!isMultiplayerOn));
+        MultiplayerToggle.isOn = GameValues.IsMultiplayer;
+
+        DifficultyToggles.transform.GetChild((int)GameValues.Difficulty).GetComponent<Toggle>().isOn = true;
+    }
+
+    public void PlayGame()
+    {
+        adsScript.ShowInterstitialAd();
+    }
+
+    public void SetMultiplayer(bool isOn)
+    {
+        GameValues.IsMultiplayer = isOn;
+    }
+
+    #region Difficulty
+    public void SetEasyDifficulty(bool isOn)
+    {
+        if (isOn)
+            GameValues.Difficulty = GameValues.Difficulties.Easy;
+    }
+
+    public void SetMediumDifficulty(bool isOn)
+    {
+        if (isOn)
+            GameValues.Difficulty = GameValues.Difficulties.Medium;
+    }
+
+    public void SetHardDifficulty(bool isOn)
+    {
+        if (isOn)
+            GameValues.Difficulty = GameValues.Difficulties.Hard;
+    }
+    #endregion
+}
